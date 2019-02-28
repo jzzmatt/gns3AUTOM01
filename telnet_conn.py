1414d5114efa591801ef4cab4cf3a3797ea06283
jzzmatt <32575597+jzzmatt@users.noreply.github.com>
2019-02-28 18:12:49 +0100

    Update telnet_conn.py

--- telnet_conn.py
+++ telnet_conn.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import telnetlib
 import sys
 import time
@@ -7,8 +6,6 @@ import threading
 
 
 def get_ip():
-    hostslists = []
-    hostDB = {}
     hosts_ip = []
     host_files = raw_input("Enter your host files\n->: ")
 
@@ -18,72 +15,59 @@ def get_ip():
 
 
     print(hosts_ip)
-
-#    if len(hostslists) != 0:
-#        for item in hostslists:
-#             hostDB[item[1]] = item[0]
-#    #print(hostDB)
-
-
-#    for name, ip in hostDB.items():
-#        hosts_ip.append(ip)
-
-
     return hosts_ip
 
-
 def open_telnet_conn(ip, command):
-     credential = {
+    timeout= 3
+    credential = {
                   'username': 'junior.mateus',
                   'password' : 'Checkk2009'
                    }
-     tn= telnetlib.Telnet(ip)
-     tn.read_until('Username:')
-     tn.write(credential['username'] + "\n")
-     time.sleep(1)
 
-     if credential['password']:
-        tn.read_until('Password:')
+    tn= telnetlib.Telnet(ip)
+    tn.read_until('Username:', timeout)
+    tn.write(credential['username'] + "\n")
+    time.sleep(1)
+
+    if credential['password']:
+        tn.read_until('Password:', timeout)
         tn.write(credential['password'] + "\n")
         time.sleep(1)
 
+    tn.write("terminal length 0\n")
+    time.sleep(1)
+    tn.write("config term\n")
+    time.sleep(1)
 
-     tn.write("terminal length 0\n")
-     time.sleep(1)
-
-
-     tn.write("config term\n")
-     time.sleep(1)
-
-
-
-     with open(command, 'r') as f_command:
-          for line in f_command.readlines():
+    with open(command, 'r') as f_command:
+            for line in f_command.readlines():
                     tn.write(line+"\n")
-                    time.sleep(1)
+                    time.sleep(2)
 
-
-     output= tn.read_very_eager()
-     print("------DISPLAY OUTPUT COMMAND ------")
-     print(output)
-     tn.close()
-     print("**___SETUP {} ____END**".format(ip))
-     time.sleep(1)
-
-
+    output= tn.read_very_eager()
+    print("------DISPLAY OUTPUT COMMAND ------")
+    print(output)
+    tn.close()
+    print("**___SETUP {} ____END**".format(ip))
+    time.sleep(1)
 
 def create_threads():
      file_commands = raw_input('Enter your file command\n->: ')
      threads = []
-     for ip in get_ip():
-          #print("Execute on ip {}".format(ip))
-          th = threading.Thread(target = open_telnet_conn, args = (ip,file_commands))
-          th.start()
-          threads.append(th)
+     try:
+         for ip in get_ip():
+             #print("Execute on ip {}".format(ip))
+             th = threading.Thread(target= open_telnet_conn, args = (ip,file_commands))
+             th.start()
+     except Exception as e:
+          with open("failed_hosts.txt", "w") as f:
+                f.write("{}\n===>{}".format(ip,e))
 
+     finally:
+         threads.append(th)
 
      for th in threads:
-               th.join()
+             th.join()
 
 if __name__=="__main__":
          create_threads()
